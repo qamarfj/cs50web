69e23cfc4f93c31df3c7306ba80456a5d8c39f29
qamarfj <qamarfj@hotmail.com>
2020-12-23 06:45:47 +0100

    impelented basic room

--- project2/application.py
+++ project2/application.py
@@ -2,7 +2,7 @@ import os
 import datetime
 
 from flask import Flask, jsonify, render_template, request
-from flask_socketio import SocketIO, emit
+from flask_socketio import SocketIO, emit, join_room, rooms
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
@@ -22,28 +22,43 @@ def index():
 @socketio.on("login")
 def login(data):
     users.append(data["userName"])
+    channel = data["activechanel"]
+    join_room(channel)
     print('newuser login')
-    emit("announce message", messages, broadcast=True)
+    userName = data["userName"]
+    newMessage = "has join room "+channel
+    currentTime = datetime.datetime.now().strftime("%H:%M:%S")
+    serverData = {'userName': userName, 'newMessage': newMessage,
+                  'messagetTime': currentTime}
+
+    emit("announce login", serverData, room=channel)
 
 
 @socketio.on("handshake")
 def handshake(data):
-    user = data["userName"]
-    activechanel = data["activechanel"]
-    emit("announce message", messages, broadcast=True)
+    channel = data["activechanel"]
+    join_room(channel)
+    print('newuser login')
+    userName = data["userName"]
+    newMessage = "has join room "+channel
+    currentTime = datetime.datetime.now().strftime("%H:%M:%S")
+    serverData = {'userName': userName, 'newMessage': newMessage,
+                  'messagetTime': currentTime}
+    messages.append(serverData)
+    emit("announce login", serverData, room=channel)
 
 
 @socketio.on("mew message")
 def vote(data, methods=['GET', 'POST']):
     userName = data["userName"]
     newMessage = data["newMessage"]
-
+    channel = data["activechanel"]
     currentTime = datetime.datetime.now().strftime("%H:%M:%S")
     serverData = {'userName': userName, 'newMessage': newMessage,
                   'messagetTime': currentTime}
     messages.append(serverData)
     print("submit vote call: ", data)
-    emit("announce message", messages, broadcast=True)
+    emit("announce message", messages, room=channel)
 
 
 if __name__ == '__main__':
--- project2/static/index.js
+++ project2/static/index.js
@@ -2,6 +2,7 @@ document.addEventListener("DOMContentLoaded", () => {
   //username is saved then remove login section
   const loginDiv = document.getElementById("login");
   let userName = localStorage.getItem("userName");
+  let activeChannel = localStorage.getItem("activeChannel");
   if (userName) loginDiv.remove();
 
   // Connect to websocket
@@ -13,20 +14,26 @@ document.addEventListener("DOMContentLoaded", () => {
     //anouse user connection
     //if user exists
     function handshake() {
-      //const activeChannel = localStorage.get("activeChannel");
       socket.emit("handshake", {
         userName: userName,
-        //activechanel: activeChannel,
+        activechanel: activeChannel,
       });
     }
     //login function
     function login() {
       document.getElementById("btn-login").addEventListener("click", () => {
-        userName = document.getElementById("user-name").value;
-        localStorage.setItem("userName", userName);
+        const user = document.getElementById("user-name").value;
+        const channelName = document.getElementById("user-channel").value;
+        //store in local
+        localStorage.setItem("userName", user);
+        localStorage.setItem("activeChannel", channelName);
+        //assign to variables
+        userName = user;
+        activeChannel = channelName;
         //call to login
         socket.emit("login", {
           userName: userName,
+          activechanel: activeChannel,
         });
         //remove login section
         loginDiv.remove();
@@ -45,6 +52,7 @@ document.addEventListener("DOMContentLoaded", () => {
       const clientData = {
         userName: userName,
         newMessage: newMessageInput.value,
+        activechanel: activeChannel,
       };
 
       socket.emit("mew message", clientData);
@@ -52,13 +60,20 @@ document.addEventListener("DOMContentLoaded", () => {
       newMessageInput.value = "";
       newMessageInput.focus();
     });
+    // When a new login is announced, add to the unordered list
+    socket.on("announce login", function (serverData) {
+      console.log(serverData);
+
+      const h3 = document.getElementById("h3");
+      if (h3) h3.remove();
+      const ul = document.getElementById("UL");
+      const newLi = document.createElement("li");
+      newLi.textContent = `${serverData.userName} : ${serverData.newMessage} at: ${serverData.messagetTime}`;
+      ul.appendChild(newLi);
+    });
     // When a new message is announced, add to the unordered list
     socket.on("announce message", function (data) {
       console.log(data);
-      if (data.length > 0) {
-        const h3 = document.getElementById("h3");
-        if (h3) h3.remove();
-      }
       const ul = document.getElementById("UL");
       while (ul.firstChild) {
         ul.removeChild(ul.firstChild);
